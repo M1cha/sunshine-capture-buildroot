737ff322d9e82993fafbada0d7ac456b9b7519ef
Michael Zimmermann <sigmaepsilon92@gmail.com>
2026-04-19 10:22:12 +0200

    cm3588: move ssh host keys to data partition
    That way they are preserved after replacing the rootfs.

--- buildroot-external-sunshinecap/board/friendlyelec/cm3588/post-build.sh
+++ buildroot-external-sunshinecap/board/friendlyelec/cm3588/post-build.sh
@@ -3,3 +3,6 @@
 BOARD_DIR="$(dirname "$0")"
 
 install -m 0644 -D "$BOARD_DIR"/extlinux.conf "$TARGET_DIR"/boot/extlinux/extlinux.conf
+
+grep -qE '^\s*Include\s+/etc/ssh/sshd_config\.d/\*\.conf' "$TARGET_DIR"/etc/ssh/sshd_config || \
+	sed -i '1i Include /etc/ssh/sshd_config.d/*.conf' "$TARGET_DIR"/etc/ssh/sshd_config
